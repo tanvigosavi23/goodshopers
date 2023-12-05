b7bc623cc6c504c8e213fb0cd6d107789bec2695
vishalant23 <1k_y_c@23>
2023-12-05 10:25:30 +0530

    add crypto js

--- src/app/shared/services/cryptojs.service.ts
+++ src/app/shared/services/cryptojs.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptojsService {
 
+  secretKey = "Pass@123#"
+
   constructor() { }
+
+
+  decryptLocalLPassStorage() {
+    if(localStorage.getItem("lpassword") == null){
+      return null
+    }else{
+      let localData = localStorage.getItem("lpassword") || '';
+      const decryptedBytes = CryptoJS.AES.decrypt(localData, "Pass@123#")
+      let data = decryptedBytes.toString(CryptoJS.enc.Utf8);
+      return JSON.parse(data)
+    }  
+  }
+
+  encryptLocalLPassStorage(data: any) {
+    if (typeof data == typeof 'String') {
+      let encryptedData = CryptoJS.AES.encrypt(data, "Pass@123#").toString();
+      localStorage.setItem("lpassword", encryptedData)
+    } else {
+      data = JSON.stringify(data)
+      let encryptedData = CryptoJS.AES.encrypt(data, "Pass@123#").toString();
+      localStorage.setItem("lpassword", encryptedData)
+    }
+  }
+
+  
 }
